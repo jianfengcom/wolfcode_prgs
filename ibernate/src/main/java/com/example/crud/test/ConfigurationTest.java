72e36ca691da60e2e766736a67318eb5ca8d5da2
chen <chen@wolfcode.cn>
2020-08-30 13:16:18 +0800

    hibernate -

--- ibernate/src/main/java/com/example/crud/test/ConfigurationTest.java
+++ ibernate/src/main/java/com/example/crud/test/ConfigurationTest.java
@@ -1,22 +1,35 @@
 package com.example.crud.test;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
-public class PropertiesReadDemo {
+public class ConfigurationTest {
     @Test
-    public void test1() { //日志: hibernate.properties not found
+    public void test() { //日志: hibernate.properties not found
         Configuration cfg = new Configuration().configure("/hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
     }
 
+    //链: https://blog.csdn.net/u012110719/article/details/41625333
     @Test
     public void testProperties() { //日志: Loaded properties from resource hibernate.properties
         Configuration cfg = new Configuration().configure("/hibernate.cfg_p.xml");
         SessionFactory factory = cfg.buildSessionFactory();
     }
 
+    //链: https://blog.csdn.net/ninelie/article/details/52654934
+    @Test
+    public void testServiceRegistry() {
+        Configuration cfg = new Configuration().configure("/hibernate.cfg.xml");
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder(); //ServiceRegistryBuilder被弃用
+        SessionFactory factory = cfg.buildSessionFactory(builder.build());
+    }
+
+    /**
+     * 硬编码
+     */
     @Test
     public void testJava() {
         Configuration cfg = new Configuration();
@@ -31,4 +44,4 @@ public class PropertiesReadDemo {
 }
 
 // *.xml 需要在代码中引入, hibernate.properties不用引入
-// Hibernate首先去找hibernate.properties配置文件(看日志可得)
+// Hibernate首先去找hibernate.properties配置文件(由日志可得)
